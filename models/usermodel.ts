573fd725eb946d99bb2b50be04f235370f2bba2e
David <ajiboladavid0963@gmail.com>
2024-06-27 13:40:47 +0100

    added wallet to schema

--- models/usermodel.ts
+++ models/usermodel.ts
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     accountBalance: {
         type: Number,
         default: 0
+    },
+    wallet: {
+        type: Number,
+        default: 0
     }
 })
 
