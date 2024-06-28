6937c94de76f6d9319ac3842fc61140e15f1312c
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-06-28 10:23:05 +0100

    Added Wallet to schema

--- models/usermodel.ts
+++ models/usermodel.ts
@@ -1,32 +1,64 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    firstName: {
-        type: String,
-        required: true
-    },
-    lastName: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    accountBalance: {
-        type: Number,
-        default: 0
-    },
-    wallet: {
-        type: Number,
-        default: 0
-    }
-})
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accountBalance: {
+    type: Number,
+    default: 0,
+  },
+  wallet: {
+    type: Number,
+    default: 0,
+  },
+  bch: {
+    type: Number,
+    default: 0,
+  },
+  bnb: {
+    type: Number,
+    default: 0,
+  },
+  btc: {
+    type: Number,
+    default: 0,
+  },
+  eth: {
+    type: Number,
+    default: 0,
+  },
+  sol: {
+    type: Number,
+    default: 0,
+  },
+  trx: {
+    type: Number,
+    default: 0,
+  },
+  usdt: {
+    type: Number,
+    default: 0,
+  },
+});
 
 const userModel = mongoose.model("User", userSchema);
 
