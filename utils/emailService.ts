8aa4938ae111c19b760ba25bac20b753a5bf02ae
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-07-03 09:27:55 +0100

    Email Verification

--- utils/emailService.ts
+++ utils/emailService.ts
@@ -10,19 +10,37 @@ const createTransporter = (email: string, password: string) => {
   });
 };
 
-// Welcome email 
+// Welcome email
 export const sendWelcomeEmail = async (userDetails: any) => {
   const { email, firstName } = userDetails;
 
-  const subject = "Welcome to Cryptonarybit!";
+  const subject = "Welcome to CryptonaryBit!";
   const text = `
-    Dear ${firstName},
+  Dear ${firstName},
+  
 
-    Welcome to cryptonarybit! We're excited to have you on board.
+Welcome to CryptonaryBit!
+  
+We're absolutely thrilled to have you join our community. At CryptonaryBit, we're passionate about providing you with the best cryptocurrency experience possible, whether you're a seasoned trader or just starting your journey.
 
-    Best regards,
-    cryptonarybit Team
-  `;
+Here's what you can look forward to as a new member
+
+
+Exclusive Insights: Stay ahead of the curve with our in-depth market analysis and expert predictions.
+
+Real-Time Updates: Never miss a beat with our instant alerts and notifications on the latest trends and movements.
+
+Comprehensive Tools: From advanced charting to personalized dashboards, we have all the tools you need to make informed decisions.
+
+Supportive Community: Connect with like-minded individuals, share strategies, and grow together in our vibrant community.
+
+We're committed to your success and are here to support you every step of the way. If you have any questions or need assistance, our dedicated support team is just a click away.
+
+Thank you for choosing CryptonaryBit. Let's embark on this exciting journey together and conquer the crypto world!
+
+  Best regards,
+  The CryptonaryBit Team.
+`;
 
   const mailOptions = {
     from: process.env.WELCOME_EMAIL_USER,
@@ -45,8 +63,12 @@ export const sendWelcomeEmail = async (userDetails: any) => {
 };
 
 //Transaction Email
-export const sendTransactionEmail = async (transactionDetails: any, userDetails: any) => {
-  const { type, amount, currency, date, status, externalWallet, duration } = transactionDetails;
+export const sendTransactionEmail = async (
+  transactionDetails: any,
+  userDetails: any
+) => {
+  const { type, amount, currency, date, status, externalWallet, duration } =
+    transactionDetails;
   const { email, firstName, lastName } = userDetails;
 
   let subject = `New ${type} transaction`;
